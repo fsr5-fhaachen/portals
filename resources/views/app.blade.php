ee2640507bcbea91d60e049614c0cfaa4611c1c5
Titus <tituskirch1999@gmail.com>
2021-09-21 20:51:58 +0200

    Add title

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -1,16 +1,19 @@
 <!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>Laravel</title>
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <script src="{{ asset('js/app.js') }}" defer></script>
+    <title>{{ config('app.name') }}</title>
 
-        <link href="{{ asset('css/app.css') }}" rel="stylesheet">
-    </head>
-    <body>
-        @inertia
-    </body>
+    <script src="{{ asset('js/app.js') }}" defer></script>
+
+    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+</head>
+
+<body>
+    @inertia
+</body>
+
 </html>
