6fba0d824211d1da9f0f3bb62ae74aafcb89773c
Titus Kirch <titus.kirch@tkirch.dev>
2022-09-01 22:33:37 +0200

    Implement CourseSeeder

--- database/seeders/CourseSeeder.php
+++ database/seeders/CourseSeeder.php
@@ -2,6 +2,7 @@
 
 namespace Database\Seeders;
 
+use App\Models\Course;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
@@ -14,6 +15,33 @@ class CourseSeeder extends Seeder
      */
     public function run()
     {
-        //
+        Course::create([
+            'name' => 'B. Eng. Elektrotechnik',
+            'abbreviation' => 'ET'
+        ]);
+        Course::create([
+            'name' => 'B. Sc. Informatik',
+            'abbreviation' => 'INF'
+        ]);
+        Course::create([
+            'name' => 'B. Sc. Media and Communications for Digital Business',
+            'abbreviation' => 'MCD'
+        ]);
+        Course::create([
+            'name' => 'B. Sc. Wirtschaftsinformatik',
+            'abbreviation' => 'WI'
+        ]);
+        Course::create([
+            'name' => 'B. Eng. Smart Building Engineering',
+            'abbreviation' => 'SBE'
+        ]);
+        Course::create([
+            'name' => 'M. Eng. Elektrotechnik',
+            'abbreviation' => 'ET-Master'
+        ]);
+        Course::create([
+            'name' => 'M. Eng. Information Systems Engineering',
+            'abbreviation' => 'ISE-Master'
+        ]);
     }
 }
