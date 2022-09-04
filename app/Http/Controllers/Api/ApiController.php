0e335102c7628b0fb6df37e6638e7311df2aa125
Titus Kirch <titus.kirch@tkirch.dev>
2022-09-04 23:42:34 +0200

    Add health route

---
+++ app/Http/Controllers/Api/ApiController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+
+class ApiController extends Controller
+{
+  /**
+   * Display the index page
+   * 
+   * @return \Illuminate\Http\JsonResponse
+   */
+  public function ping()
+  {
+     return response()->json(['message' => 'pong']);
+  }
+}
