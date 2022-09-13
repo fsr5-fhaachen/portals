f1aa870df5d04ec24438512cf1006d7b43e63277
Titus Kirch <titus.kirch@tkirch.dev>
2022-09-13 23:32:14 +0200

    Add missing name

--- database/migrations/2022_08_24_085154_create_groups_table.php
+++ database/migrations/2022_08_24_085154_create_groups_table.php
@@ -14,6 +14,7 @@ return new class () extends Migration {
     {
         Schema::create('groups', function (Blueprint $table) {
             $table->id();
+            $table->string('name', 255)->nullable(false);
             $table->timestamps();
             $table->foreignId('event_id')->nullable(false)->constrained()->onUpdate('cascade')->onDelete('cascade');
             $table->foreignId('course_id')->nullable(true)->constrained()->onUpdate('cascade')->onDelete('cascade');
