d1bb2f580470a324afbfc8d5e2f54a4dd9a83419
Benedikt Haas <benedikt-haas96@gmx.de>
2022-09-18 12:54:54 +0200

    Implemented stresstest seeder

---
+++ database/seeders/StresstestSeeder.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Registration;
+use App\Models\User;
+use Faker\Factory;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class StresstestSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $course_id = env('SEEDER_COURSE_ID', 1);
+        $event_id = env('SEEDER_EVENT_ID', 1);
+        $slot_id = env('SEEDER_SLOT_ID', null);
+        $amount = env('SEEDER_AMOUNT', 1);
+        $nondrinker_percentage = env('SEEDER_ND_PERCENTAGE');
+
+        $faker = Factory::create();
+
+        User::factory()
+          ->has(
+            Registration::factory()
+              ->state([
+                'event_id' => (int)$event_id,
+                'slot_id' => (int)$slot_id,
+                'group_id' => null,
+                'drinks_alcohol' => $faker->boolean((int)$nondrinker_percentage),
+                'fulfils_requirements' => false,
+                'is_present' => false,
+                'queue_position' => -1
+              ])
+              ->count(1)
+          )
+          ->state(['course_id' => (int)$course_id])
+          ->count((int)$amount)
+          ->create();
+    }
+}
