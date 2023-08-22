72278e52b9968b0214bca50f7d6d8fa8573c230d
Titus Kirch <titus.kirch@kirch.dev>
2023-08-22 02:15:26 +0200

    Lint

--- resources/js/types/components.d.ts
+++ resources/js/types/components.d.ts
@@ -5,47 +5,47 @@
 // Read more: https://github.com/vuejs/core/pull/3399
 export {}
 
-declare module 'vue' {
+declare module "vue" {
   export interface GlobalComponents {
-    2: typeof import("./../components/ui/Dl copy 2.vue")["default"]
-    AppButton: typeof import('./../components/app/Button.vue')['default']
-    AppLink: typeof import('./../components/app/Link.vue')['default']
-    AppMessage: typeof import('./../components/app/Message.vue')['default']
-    AppNavbar: typeof import('./../components/app/Navbar.vue')['default']
-    BoxContainer: typeof import('./../components/box/Container.vue')['default']
-    Card: typeof import("./../layouts/Card.vue")["default"]
-    CardBase: typeof import('./../components/card/Base.vue')['default']
-    CardCard: typeof import("./../components/card/Card.vue")["default"]
-    CardContainer: typeof import('./../components/card/Container.vue')['default']
-    CardLayout: typeof import('./../layouts/CardLayout.vue')['default']
-    copy: typeof import("./../components/ui/Dl copy.vue")["default"]
-    copyTable: typeof import("./../components/slot copy/Table.vue")["default"]
-    CourseBox: typeof import('./../components/course/Box.vue')['default']
-    DashboardCardLayout: typeof import('./../layouts/DashboardCardLayout.vue')['default']
-    DashboardLayout: typeof import('./../layouts/DashboardLayout.vue')['default']
-    Default: typeof import("./../layouts/Default.vue")["default"]
-    DefaultLayout: typeof import('./../layouts/DefaultLayout.vue')['default']
-    EventCard: typeof import('./../components/event/Card.vue')['default']
-    EventTable: typeof import('./../components/event/Table.vue')['default']
-    FormContainer: typeof import('./../components/form/Container.vue')['default']
-    FormDivider: typeof import('./../components/form/Divider.vue')['default']
-    FormRow: typeof import('./../components/form/Row.vue')['default']
-    FormSchema: typeof import('./../components/form/Schema.vue')['default']
-    GgroupTable: typeof import("./../components/ggroup/Table.vue")["default"]
-    GirdContainer: typeof import('./../components/gird/Container.vue')['default']
-    GroupTable: typeof import('./../components/group/Table.vue')['default']
-    LayoutDashboardContent: typeof import('./../components/layout/DashboardContent.vue')['default']
-    RegistrationTable: typeof import('./../components/registration/Table.vue')['default']
-    SlotsTable: typeof import("./../components/slots/Table.vue")["default"]
-    SlotTable: typeof import('./../components/slot/Table.vue')['default']
-    UiDateString: typeof import('./../components/ui/DateString.vue')['default']
-    UiDateTimeString: typeof import('./../components/ui/DateTimeString.vue')['default']
-    UiDd: typeof import('./../components/ui/Dd.vue')['default']
-    UiDl: typeof import('./../components/ui/Dl.vue')['default']
-    UiDt: typeof import('./../components/ui/Dt.vue')['default']
-    UiH1: typeof import('./../components/ui/H1.vue')['default']
-    UiH2: typeof import('./../components/ui/H2.vue')['default']
-    UiMessage: typeof import('./../components/ui/Message.vue')['default']
-    UiTimeString: typeof import('./../components/ui/TimeString.vue')['default']
+    2: typeof import("./../components/ui/Dl copy 2.vue")["default"];
+    AppButton: typeof import("./../components/app/Button.vue")["default"];
+    AppLink: typeof import("./../components/app/Link.vue")["default"];
+    AppMessage: typeof import("./../components/app/Message.vue")["default"];
+    AppNavbar: typeof import("./../components/app/Navbar.vue")["default"];
+    BoxContainer: typeof import("./../components/box/Container.vue")["default"];
+    Card: typeof import("./../layouts/Card.vue")["default"];
+    CardBase: typeof import("./../components/card/Base.vue")["default"];
+    CardCard: typeof import("./../components/card/Card.vue")["default"];
+    CardContainer: typeof import("./../components/card/Container.vue")["default"];
+    CardLayout: typeof import("./../layouts/CardLayout.vue")["default"];
+    copy: typeof import("./../components/ui/Dl copy.vue")["default"];
+    copyTable: typeof import("./../components/slot copy/Table.vue")["default"];
+    CourseBox: typeof import("./../components/course/Box.vue")["default"];
+    DashboardCardLayout: typeof import("./../layouts/DashboardCardLayout.vue")["default"];
+    DashboardLayout: typeof import("./../layouts/DashboardLayout.vue")["default"];
+    Default: typeof import("./../layouts/Default.vue")["default"];
+    DefaultLayout: typeof import("./../layouts/DefaultLayout.vue")["default"];
+    EventCard: typeof import("./../components/event/Card.vue")["default"];
+    EventTable: typeof import("./../components/event/Table.vue")["default"];
+    FormContainer: typeof import("./../components/form/Container.vue")["default"];
+    FormDivider: typeof import("./../components/form/Divider.vue")["default"];
+    FormRow: typeof import("./../components/form/Row.vue")["default"];
+    FormSchema: typeof import("./../components/form/Schema.vue")["default"];
+    GgroupTable: typeof import("./../components/ggroup/Table.vue")["default"];
+    GirdContainer: typeof import("./../components/gird/Container.vue")["default"];
+    GroupTable: typeof import("./../components/group/Table.vue")["default"];
+    LayoutDashboardContent: typeof import("./../components/layout/DashboardContent.vue")["default"];
+    RegistrationTable: typeof import("./../components/registration/Table.vue")["default"];
+    SlotsTable: typeof import("./../components/slots/Table.vue")["default"];
+    SlotTable: typeof import("./../components/slot/Table.vue")["default"];
+    UiDateString: typeof import("./../components/ui/DateString.vue")["default"];
+    UiDateTimeString: typeof import("./../components/ui/DateTimeString.vue")["default"];
+    UiDd: typeof import("./../components/ui/Dd.vue")["default"];
+    UiDl: typeof import("./../components/ui/Dl.vue")["default"];
+    UiDt: typeof import("./../components/ui/Dt.vue")["default"];
+    UiH1: typeof import("./../components/ui/H1.vue")["default"];
+    UiH2: typeof import("./../components/ui/H2.vue")["default"];
+    UiMessage: typeof import("./../components/ui/Message.vue")["default"];
+    UiTimeString: typeof import("./../components/ui/TimeString.vue")["default"];
   }
 }
