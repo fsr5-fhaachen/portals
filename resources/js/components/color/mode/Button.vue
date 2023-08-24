1cb0acbf5ec1acb5ab16af879f586d106729097f
Titus Kirch <titus.kirch@kirch.dev>
2023-08-24 22:02:00 +0200

    Add Darkmode (Improve Darkmode button) #172

--- resources/js/components/color/mode/Button.vue
+++ resources/js/components/color/mode/Button.vue
@@ -1,11 +1,10 @@
 <template>
   <div
     @click="toggleColorMode()"
-    class="text-gray-500 hover:cursor-pointer hover:text-fhac-mint-dark"
+    class="text-gray-500 hover:cursor-pointer hover:text-fhac-mint-dark dark:text-gray-400 dark:hover:text-fhac-mint"
   >
     <FontAwesomeIcon
       :icon="getColorMode() == 'dark' ? ['fas', 'sun'] : ['fas', 'moon']"
-      class="mr-2 w-8"
     />
   </div>
 </template>
