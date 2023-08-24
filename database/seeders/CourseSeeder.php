827597270bd0005750b24499a61ff99f1bacbf43
Titus Kirch <titus.kirch@kirch.dev>
2023-08-24 22:20:33 +0200

    Update seeder

--- database/seeders/CourseSeeder.php
+++ database/seeders/CourseSeeder.php
@@ -17,42 +17,43 @@ class CourseSeeder extends Seeder
                 'name' => 'B. Eng. Elektrotechnik',
                 'abbreviation' => 'ET',
                 'icon' => 'fa-plug',
-                'color' => 'rgb(234 179 8)',
+                'classes' => 'bg-yellow-500 text-white dark:bg-yellow-600',
                 'show_on_registration' => true,
             ], [
                 'name' => 'B. Sc. Informatik',
                 'abbreviation' => 'INF',
                 'icon' => 'fa-laptop-code',
-                'color' => 'rgb(29 78 216)',
+                'classes' => 'bg-blue-700 text-white dark:bg-blue-800',
                 'show_on_registration' => true,
             ], [
                 'name' => 'B. Sc. Media and Communications for Digital Business',
                 'abbreviation' => 'MCD',
                 'icon' => 'fa-paint-brush',
-                'color' => 'rgb(91 33 182)',
+                'classes' => 'bg-violet-800 text-white dark:bg-violet-900',
                 'show_on_registration' => false,
             ], [
                 'name' => 'B. Sc. Wirtschaftsinformatik',
                 'abbreviation' => 'WI',
                 'icon' => 'fa-sack-dollar',
-                'color' => 'rgb(22 101 52)',
+                'classes' => 'bg-green-800 text-white dark:bg-green-900',
                 'show_on_registration' => true,
             ], [
                 'name' => 'B. Eng. Smart Building Engineering',
                 'abbreviation' => 'SBE',
                 'icon' => 'fa-house-signal',
-                'color' => 'rgb(41 37 36)',
+                'classes' => 'bg-stone-800 text-white dark:bg-stone-900',
                 'show_on_registration' => false,
             ], [
                 'name' => 'M. Eng. Elektrotechnik',
                 'abbreviation' => 'ET-Master',
                 'icon' => 'fa-person-through-window',
-                'color' => 'rgb(239 68 68)',
+                'classes' => 'bg-red-500 text-white dark:bg-red-600',
+                'show_on_registration' => false,
             ], [
                 'name' => 'M. Eng. Information Systems Engineering',
                 'abbreviation' => 'ISE-Master',
                 'icon' => 'fa-bug',
-                'color' => 'rgb(14 116 144)',
+                'classes' => 'bg-cyan-700 text-white dark:bg-cyan-800',
                 'show_on_registration' => true,
             ],
         ];
@@ -69,7 +70,8 @@ class CourseSeeder extends Seeder
             $newCourse->name = $course['name'];
             $newCourse->abbreviation = $course['abbreviation'];
             $newCourse->icon = $course['icon'];
-            $newCourse->color = $course['color'];
+            $newCourse->classes = $course['classes'];
+            $newCourse->show_on_registration = $course['show_on_registration'];
 
             // save the course
             $newCourse->save();
