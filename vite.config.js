4d41c738ba51275e7fe31397df78ad288102efe6
Titus Kirch <titus.kirch@kirch.dev>
2023-08-24 21:28:13 +0200

    Format file

--- vite.config.js
+++ vite.config.js
@@ -11,10 +11,10 @@ export default defineConfig({
     },
   },
   server: {
-    host: '0.0.0.0',
+    host: "0.0.0.0",
     hmr: {
-        host: 'localhost'
-    }
+      host: "localhost",
+    },
   },
   plugins: [
     laravel({
