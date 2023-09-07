c28a6a1ea0f2c0999d7988c2d1836ac5126d265d
Titus Kirch <titus.kirch@kirch.dev>
2023-09-08 00:29:07 +0200

    Add missing hetzner button

--- resources/js/layouts/DefaultLayout.vue
+++ resources/js/layouts/DefaultLayout.vue
@@ -1,10 +1,40 @@
 <template>
-  <div>
-    <slot />
+  <div class="flex h-full flex-col">
+    <div class="flex-grow">
+      <slot />
+    </div>
+
+    <div class="mt-6 overflow-hidden pb-12">
+      <div
+        class="mt-4 flex items-center justify-center px-2 text-sm text-gray-500"
+      >
+        <AppLink :href="packageRepositoryUrl" theme="gray">
+          Powered by {{ packageName }}
+        </AppLink>
+      </div>
+      <div class="mt-6 px-2 text-sm text-gray-500">
+        <AppLink
+          href="https://www.hetzner.com/"
+          theme="none"
+          class="flex flex-col items-center justify-center gap-2"
+        >
+          <span>Hosted by</span>
+          <img
+            class=""
+            src="/images/hetzner.png"
+            alt="Hetzner"
+            loading="lazy"
+          />
+        </AppLink>
+      </div>
+    </div>
   </div>
 </template>
 
 <script setup lang="ts">
 const { initColorMode } = useColorMode();
 initColorMode();
+
+const packageName = __PACKAGE_NAME__;
+const packageRepositoryUrl = __PACKAGE_REPOSITORY_URL__;
 </script>
