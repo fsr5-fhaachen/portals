6201de8181921821f19691f14749d092a789768a
Titus Kirch <requests@tkirch.dev>
2023-09-13 16:40:16 +0200

    Update database.php

--- config/database.php
+++ config/database.php
@@ -156,7 +156,7 @@ return [
                     'password' => env('REDIS_PASSWORD'),
                     'port' => env('REDIS_PORT', '6379'),
                     'database' => env('REDIS_DB', '0'),
-                    'prefix' => '',
+                    'prefix' => 'default',
                 ],
             ],
 
