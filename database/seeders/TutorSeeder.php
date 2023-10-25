fbcd4b06f828f960d7a439ff9eb98871f7d914df
Simon Ostendorf <github@simon-ostendorf.de>
2023-10-25 13:26:30 +0200

    feat(database/seeders/tutor): switch delimiter and add disabled (#521)

--- database/seeders/TutorSeeder.php
+++ database/seeders/TutorSeeder.php
@@ -13,7 +13,7 @@ class TutorSeeder extends Seeder
      *
      * @var string
      */
-    private const TUTORS_CSV_PATH = __DIR__.'/tutors.csv';
+    private const TUTORS_CSV_PATH = __DIR__ . '/tutors.csv';
 
     /**
      * Run the tutor seeds.
@@ -21,7 +21,7 @@ class TutorSeeder extends Seeder
     public function run(): void
     {
         // check if the file exists
-        if (! file_exists(self::TUTORS_CSV_PATH)) {
+        if (!file_exists(self::TUTORS_CSV_PATH)) {
             return;
         }
 
@@ -51,26 +51,38 @@ class TutorSeeder extends Seeder
             }
 
             // create a new user
-            echo 'Creating tutor '.$tutor[0].' '.$tutor[1].' ('.$tutor[3].')'.PHP_EOL;
+            echo 'Creating tutor ' . $tutor[0] . ' ' . $tutor[1] . ' (' . $tutor[3] . ')' . PHP_EOL;
             $user = new User();
             $user->lastname = $tutor[0];
             $user->firstname = $tutor[1];
             $user->course_id = $courseByKey[$tutor[2]]->id;
             $user->email = $tutor[3];
 
+            //set user to disabled
+            if (array_key_exists(5, $tutor) && $tutor[5] == '1') {
+                echo 'Setting tutor to disabled for ' . $tutor[0] . ' ' . $tutor[1] . ' (' . $tutor[3] . ')' . PHP_EOL;
+                $user->is_disabled = true;
+            }
+
             // save the user
             $user->save();
 
             // assigne user role
+            echo 'Assigning role tutor to ' . $tutor[0] . ' ' . $tutor[1] . ' (' . $tutor[3] . ')' . PHP_EOL;
             $user->assignRole('tutor');
 
             // check if the user has additional roles
             if (array_key_exists(4, $tutor)) {
                 // split roles by comma
-                $roles = explode(',', $tutor[4]);
+                $roles = explode('|', $tutor[4]);
 
                 // loop through the roles
                 foreach ($roles as $role) {
+                    // skip empty strings because of ";;" in the csv file
+                    if (empty($role)) {
+                        continue;
+                    }
+                    echo 'Assigning role ' . $role . ' to ' . $tutor[0] . ' ' . $tutor[1] . ' (' . $tutor[3] . ')' . PHP_EOL;
                     $user->assignRole($role);
                 }
             }
