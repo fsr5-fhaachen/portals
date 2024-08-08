132813957293ec34d686a99b492e5fef46023358
TLE931 <tom.lersmacher12@gmail.com>
2024-08-08 16:15:01 +0200

    added php unit

---
+++ tests/Feature/GroupDivisionTest.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace Tests\Feature;
+
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Tests\TestCase;
+
+use function PHPUnit\Framework\assertTrue;
+
+class GroupDivisionTest extends TestCase
+{
+    use RefreshDatabase;
+
+    /**
+     * A basic feature test example.
+     */
+    public function test_example(): void
+    {
+        assertTrue(true);
+    }
+}
