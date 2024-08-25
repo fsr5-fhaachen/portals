6b1fa6979fc2a08644a8446ba62292d0e0c4ec24
Shift <shift@laravelshift.com>
2024-08-25 18:04:29 +0000

    Consolidate service providers

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,11 +2,24 @@
 
 namespace App\Providers;
 
+use Illuminate\Cache\RateLimiting\Limit;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\RateLimiter;
+use Illuminate\Support\Facades\Route;
 use Illuminate\Support\Facades\URL;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
 {
+    /**
+     * The path to the "home" route for your application.
+     *
+     * This is used by Laravel authentication to redirect users after login.
+     *
+     * @var string
+     */
+    public const HOME = '/dashboard';
+
     /**
      * Register any application services.
      */
@@ -24,5 +37,19 @@ class AppServiceProvider extends ServiceProvider
         if (config('app.force_https')) {
             URL::forceScheme('https');
         }
+
+        $this->bootRoute();
+    }
+
+    public function bootRoute(): void
+    {
+        // check if app debug mode is not enabled and then apply rate limiter
+        if (! config('app.debug')) {
+            RateLimiter::for('api', function (Request $request) {
+                return Limit::perMinute(1200)->by($request->user()?->id ?: $request->ip());
+            });
+        }
+
+
     }
 }
