b175a442df6640e7539af22f8a3ab48db34d1ea6
Simon Ostendorf <github@simon-ostendorf.de>
2024-09-07 10:35:01 +0200

    fix: prod deployment

--- config/database.php
+++ config/database.php
@@ -24,32 +24,26 @@ return [
         'options' => [
             'cluster' => env('REDIS_CLUSTER', 'redis'),
             'prefix' => env('REDIS_PREFIX', Str::slug(env('APP_NAME', 'laravel'), '_').'_database_'),
+        ],
+
+        'default' => [
+            'url' => env('REDIS_URL'),
+            'host' => env('REDIS_HOST', '127.0.0.1'),
+            'username' => env('REDIS_USERNAME'),
             'password' => env('REDIS_PASSWORD'),
+            'port' => env('REDIS_PORT', '6379'),
+            'database' => env('REDIS_DB', '0'),
         ],
 
-        'clusters' => [
-            'default' => [
-                [
-                    'host' => env('REDIS_HOST', '127.0.0.1'),
-                    'username' => env('REDIS_USERNAME'),
-                    'password' => env('REDIS_PASSWORD'),
-                    'port' => env('REDIS_PORT', '6379'),
-                    'database' => env('REDIS_DB', '0'),
-                    'prefix' => '',
-                ],
-            ],
-
-            'cache' => [
-                [
-                    'host' => env('REDIS_HOST', '127.0.0.1'),
-                    'username' => env('REDIS_USERNAME'),
-                    'password' => env('REDIS_PASSWORD'),
-                    'port' => env('REDIS_PORT', '6379'),
-                    'database' => env('REDIS_CACHE_DB', '1'),
-                    'prefix' => 'cache',
-                ],
-            ],
+        'cache' => [
+            'url' => env('REDIS_URL'),
+            'host' => env('REDIS_HOST', '127.0.0.1'),
+            'username' => env('REDIS_USERNAME'),
+            'password' => env('REDIS_PASSWORD'),
+            'port' => env('REDIS_PORT', '6379'),
+            'database' => env('REDIS_CACHE_DB', '1'),
         ],
+
     ],
 
 ];
