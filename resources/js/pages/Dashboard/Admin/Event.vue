a10313aaa5a2f4b8845f7a148bbc54ca4fb1e2c0
nd9824s <nd9824s@ad.fh-aachen.de>
2025-08-19 11:54:38 +0200

    no 204 message will be send anymore, because we will always have the user count data, so we dont need the extra infor in the header for different display style

--- resources/js/pages/Dashboard/Admin/Event.vue
+++ resources/js/pages/Dashboard/Admin/Event.vue
@@ -162,10 +162,6 @@ const fetchStatistics = async () => {
   });
 
   if (response.ok) {
-    if( response.status === 204) {
-      statistics.value = [];
-      return;
-    }
     const data = await response.json();
     statistics.value = data;
   }
