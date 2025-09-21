4295209d1998136eb8b15b41c05947abef4224d6
Tom Lersmacher <161459207+TLE931@users.noreply.github.com>
2025-09-21 02:20:53 +0200

    fix: Middleware didn't check super admin role (#907)

--- app/Http/Middleware/IsLoggedInAdmin.php
+++ app/Http/Middleware/IsLoggedInAdmin.php
@@ -8,25 +8,25 @@ use Symfony\Component\HttpFoundation\Response;
 
 class IsLoggedInAdmin
 {
-    /**
-     * Handle an incoming request.
-     */
-    public function handle(Request $request, Closure $next): Response
-    {
-        // get auth user
-        $user = $request->user();
+  /**
+   * Handle an incoming request.
+   */
+  public function handle(Request $request, Closure $next): Response
+  {
+    // get auth user
+    $user = $request->user();
 
-        // check if user is not an admin
-        if (! $user->hasRole(['admin'])) {
-          return redirect()->route('dashboard.index');
-        }
-
-        // check if user is not logged in as tutor
-        if (! $request->session()->has('tutor')) {
-            session(['url.intended' => url()->current()]);
-            return redirect()->route('dashboard.tutor.login');
-        }
-
-      return $next($request);
+    // check if user is not an admin
+    if (! $user->hasRole(['admin']) && !$user->hasRole(['super admin'])) {
+      return redirect()->route('dashboard.index');
     }
+
+    // check if user is not logged in as tutor
+    if (! $request->session()->has('tutor')) {
+      session(['url.intended' => url()->current()]);
+      return redirect()->route('dashboard.tutor.login');
+    }
+
+    return $next($request);
+  }
 }
