6695bc3854fe8d111cbf7e176c7cb0f1a7b54c07
Simon Ostendorf <github@simon-ostendorf.de>
2025-10-09 23:46:35 +0200

    feat: seed avatar images

--- database/seeders/StudentSeeder.php
+++ database/seeders/StudentSeeder.php
@@ -5,6 +5,9 @@ namespace Database\Seeders;
 use App\Models\Course;
 use App\Models\User;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Http;
+use Illuminate\Support\Facades\Storage;
+use Illuminate\Support\Str;
 
 class StudentSeeder extends Seeder
 {
@@ -13,7 +16,21 @@ class StudentSeeder extends Seeder
      *
      * @var string
      */
-    private const STUDENTS_CSV_PATH = __DIR__.'/students.csv';
+    private const STUDENTS_CSV_PATH = __DIR__ . '/students.csv';
+
+    /**
+     * Set path to the folder with student images.
+     * 
+     * @var string
+     */
+    private const STUDENT_IMAGES_PATH = __DIR__ . '/images';
+
+    /**
+     * Set possible file extensions for the student images.
+     * 
+     * @var array
+     */
+    private const STUDENT_IMAGE_EXTENSIONS = ['jpg', 'jpeg', 'png'];
 
     /**
      * Run the tutor seeds.
@@ -56,6 +73,51 @@ class StudentSeeder extends Seeder
             $user->firstname = $student[1];
             $user->course_id = $courseByKey[$student[2]]->id;
             $user->email = strtolower($student[3]);
+
+            // check if an image with the name of the user in one of the possible extensions exists
+            $imageBasePath = self::STUDENT_IMAGES_PATH . '/' . strtolower($user->firstname) . '_' . strtolower($user->lastname);
+            $imageExtension = null;
+            $imagePath = null;
+            foreach (self::STUDENT_IMAGE_EXTENSIONS as $extension) {
+                if (file_exists($imageBasePath . '.' . $extension)) {
+                    $imageExtension = $extension;
+                    $imagePath = $imageBasePath . '.' . $extension;
+                    break;
+                }
+            }
+
+            if ($imagePath) {
+                // generate presigned url
+                $uuid = Str::uuid()->toString();
+                $path = 'avatars/' . $uuid . '.' . $imageExtension;
+                $presignedUrl = Storage::disk('s3')->temporaryUploadUrl(
+                    $path,
+                    now()->addMinutes(5)
+                );
+
+                // extract relevant data from presigned url
+                $uploadUrl = $presignedUrl['url'] ?? null;
+                $uploadHeaders = $presignedUrl['headers'] ?? [];
+
+                // get mime type of image
+                $mime = mime_content_type($imagePath);
+
+                // upload avatar using presigned url
+                $req = Http::withBody(file_get_contents($imagePath), $mime);
+                if (! empty($uploadHeaders)) {
+                    $req = $req->withHeaders($uploadHeaders);
+                }
+                $response = $req->put($uploadUrl);
+
+                if ($response->successful()) {
+                    // save avatar path to user
+                    $user->avatar = $path;
+
+                    $this->command->info("Found and uploaded avatar for user {$user->firstname} {$user->lastname}");
+                } else {
+                    $this->command->warn("Failed to upload avatar for user {$user->firstname} {$user->lastname}");
+                }
+            }
 
             // save the user
             $user->save();
--- database/seeders/TutorSeeder.php
+++ database/seeders/TutorSeeder.php
@@ -5,6 +5,9 @@ namespace Database\Seeders;
 use App\Models\Course;
 use App\Models\User;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Http;
+use Illuminate\Support\Facades\Storage;
+use Illuminate\Support\Str;
 
 class TutorSeeder extends Seeder
 {
@@ -13,7 +16,21 @@ class TutorSeeder extends Seeder
      *
      * @var string
      */
-    private const TUTORS_CSV_PATH = __DIR__.'/tutors.csv';
+    private const TUTORS_CSV_PATH = __DIR__ . '/tutors.csv';
+
+    /**
+     * Set path to the folder with tutor images.
+     * 
+     * @var string
+     */
+    private const TUTOR_IMAGES_PATH = __DIR__ . '/images';
+
+    /**
+     * Set possible file extensions for the tutor images.
+     * 
+     * @var array
+     */
+    private const TUTOR_IMAGE_EXTENSIONS = ['jpg', 'jpeg', 'png'];
 
     /**
      * Run the tutor seeds.
@@ -51,7 +68,7 @@ class TutorSeeder extends Seeder
             }
 
             // create a new user
-            echo 'Creating tutor '.$tutor[0].' '.$tutor[1].' ('.$tutor[3].')'.PHP_EOL;
+            echo 'Creating tutor ' . $tutor[0] . ' ' . $tutor[1] . ' (' . $tutor[3] . ')' . PHP_EOL;
             $user = new User;
             $user->lastname = $tutor[0];
             $user->firstname = $tutor[1];
@@ -60,15 +77,60 @@ class TutorSeeder extends Seeder
 
             //set user to disabled
             if (array_key_exists(5, $tutor) && $tutor[5] == '1') {
-                echo 'Setting tutor to disabled for '.$tutor[0].' '.$tutor[1].' ('.$tutor[3].')'.PHP_EOL;
+                echo 'Setting tutor to disabled for ' . $tutor[0] . ' ' . $tutor[1] . ' (' . $tutor[3] . ')' . PHP_EOL;
                 $user->is_disabled = true;
+            }
+
+            // check if an image with the name of the user in one of the possible extensions exists
+            $imageBasePath = self::TUTOR_IMAGES_PATH . '/' . strtolower($user->firstname) . '_' . strtolower($user->lastname);
+            $imageExtension = null;
+            $imagePath = null;
+            foreach (self::TUTOR_IMAGE_EXTENSIONS as $extension) {
+                if (file_exists($imageBasePath . '.' . $extension)) {
+                    $imageExtension = $extension;
+                    $imagePath = $imageBasePath . '.' . $extension;
+                    break;
+                }
+            }
+
+            if ($imagePath) {
+                // generate presigned url
+                $uuid = Str::uuid()->toString();
+                $path = 'avatars/' . $uuid . '.' . $imageExtension;
+                $presignedUrl = Storage::disk('s3')->temporaryUploadUrl(
+                    $path,
+                    now()->addMinutes(5)
+                );
+
+                // extract relevant data from presigned url
+                $uploadUrl = $presignedUrl['url'] ?? null;
+                $uploadHeaders = $presignedUrl['headers'] ?? [];
+
+                // get mime type of image
+                $mime = mime_content_type($imagePath);
+
+                // upload avatar using presigned url
+                $req = Http::withBody(file_get_contents($imagePath), $mime);
+                if (! empty($uploadHeaders)) {
+                    $req = $req->withHeaders($uploadHeaders);
+                }
+                $response = $req->put($uploadUrl);
+
+                if ($response->successful()) {
+                    // save avatar path to user
+                    $user->avatar = $path;
+
+                    $this->command->info("Found and uploaded avatar for user {$user->firstname} {$user->lastname}");
+                } else {
+                    $this->command->warn("Failed to upload avatar for user {$user->firstname} {$user->lastname}");
+                }
             }
 
             // save the user
             $user->save();
 
             // assigne user role
-            echo 'Assigning role tutor to '.$tutor[0].' '.$tutor[1].' ('.$tutor[3].')'.PHP_EOL;
+            echo 'Assigning role tutor to ' . $tutor[0] . ' ' . $tutor[1] . ' (' . $tutor[3] . ')' . PHP_EOL;
             $user->assignRole('tutor');
 
             // check if the user has additional roles
@@ -82,7 +144,7 @@ class TutorSeeder extends Seeder
                     if (empty($role)) {
                         continue;
                     }
-                    echo 'Assigning role '.$role.' to '.$tutor[0].' '.$tutor[1].' ('.$tutor[3].')'.PHP_EOL;
+                    echo 'Assigning role ' . $role . ' to ' . $tutor[0] . ' ' . $tutor[1] . ' (' . $tutor[3] . ')' . PHP_EOL;
                     $user->assignRole($role);
                 }
             }
