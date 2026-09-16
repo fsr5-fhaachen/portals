df866a4e42c39a15627177ca601f31f188666736
Timo <>
2026-09-16 11:35:27 +0200

    Revert "Button fix and PHP 8.4.25  continue bug fixed."
    This reverts commit fcde2dce1019cf0e17e6d78269a65d0e3dab757c.

--- resources/js/components/app/Button.vue
+++ resources/js/components/app/Button.vue
@@ -1,5 +1,5 @@
 <template>
-  <div :class="rootClasses" @click="handleClick">
+  <div :class="rootClasses">
     <a>
       <slot />
     </a>
@@ -20,17 +20,6 @@ const props = defineProps({
       ["default", "gray", "warning", "danger"].includes(value),
   },
 });
-
-const emit = defineEmits<{
-  (e: "click", event: MouseEvent): void;
-}>();
-
-function handleClick(event: MouseEvent) {
-  if (props.disabled) {
-    return;
-  }
-  emit("click", event);
-}
 
 const rootClasses = computed(() => {
   const classes: string[] = [];
