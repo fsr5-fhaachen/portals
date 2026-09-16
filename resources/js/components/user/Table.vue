b4e1404504676ac78e8d2980c7080b6718f0d0d2
Timo <>
2026-09-16 02:33:18 +0200

    Sorted and Colored Roles view
    Add: Roles class for colored background for each role. Roles getting sorted, before displaying.

--- resources/js/components/user/Table.vue
+++ resources/js/components/user/Table.vue
@@ -46,6 +46,7 @@ import { ref, PropType, watch } from "vue";
 import { TableColumn } from "../../types/table-column";
 import { TableFunction } from "../../types/table-function";
 import { TableButton } from "../../types/table-button";
+import { roleColors } from "../../roleColors";
 
 const props = defineProps({
   user: {
@@ -161,11 +162,12 @@ function getColumns(): Array<TableColumn> {
       let ret = "";
       if (user.roles.length) {
         ret += '<div class="flex flex-col gap-2">';
-        for (const role of user.roles) {
+        for (const role of user.roles.sort((a, b) => a.name.localeCompare(b.name))) {
+          const color = roleColors[role.name] ?? "bg-slate-500";
+
           ret +=
-            '<span class="rounded-md bg-slate-900 p-1 text-xs text-white">' +
-            role.name +
-            "</span>";
+            '<span class="rounded-md ' + color + ' text-xs text-white text-center">' +
+            role.name + "</span>";
         }
       }
       return ret;
---
+++ resources/js/roleColors.ts
@@ -0,0 +1,8 @@
+export const roleColors: Record<string, string> = {
+  tutor: "bg-blue-500",
+  "stage tutor": "bg-purple-500",
+  esa: "bg-green-500",
+  admin: "bg-red-500",
+  special: "bg-yellow-500",
+  "super admin": "bg-gradient-to-b from-yellow-300 via-purple-500 to-gray-900",
+};
